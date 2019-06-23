5008686c8b17f1d195aeeb1f59dde72493c37f5e
alexandrrusanov <Darkelf3001>
2019-06-23 15:42:12 +0300

    hw8 minor fix

--- js/script-hw8.js
+++ js/script-hw8.js
@@ -1,6 +1,11 @@
 const priceInput = document.querySelector('#priceInput');
 
+priceInput.onfocus = (event) => {
+    event.target.style.cssText = "border: 5px green solid;";
+};
+
 priceInput.onblur = (event) => {
+    event.target.style.cssText = "";
     if (event.target.value < 0) {
         event.target.style.cssText = "border: 5px red solid;";
 
@@ -14,28 +19,29 @@ priceInput.onblur = (event) => {
             correctPrice.remove();
         }, 3000);
     } else {
-        event.target.style.cssText = "border: 5px green solid;" +
-                                     "color: green;";
+        if (event.target.value !== "") {
+            event.target.style.cssText = "color: green;";
 
-        const currentPrice = document.createElement('span');
-        currentPrice.innerText = `Текущая цена: ${event.target.value}`;
+            const currentPrice = document.createElement('span');
+            currentPrice.innerText = `Текущая цена: ${event.target.value}`;
 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.innerText = "X";
-        deleteBtn.style.borderRadius = "50%";
+            const deleteBtn = document.createElement('button');
+            deleteBtn.innerText = "X";
+            deleteBtn.style.borderRadius = "50%";
 
-        let currentPriceWrapper = document.createElement('div');
-        currentPriceWrapper.appendChild(currentPrice);
-        currentPriceWrapper.appendChild(deleteBtn);
+            let currentPriceWrapper = document.createElement('div');
+            currentPriceWrapper.appendChild(currentPrice);
+            currentPriceWrapper.appendChild(deleteBtn);
 
-        const inputLabel = document.querySelector('label');
-        document.body.insertBefore(currentPriceWrapper, inputLabel);
+            const inputLabel = document.querySelector('label');
+            document.body.insertBefore(currentPriceWrapper, inputLabel);
 
-        deleteBtn.onclick = (e) => {
-            deleteBtn.parentNode.remove();
-            event.target.style.border = "";
-            event.target.style.color = "black";
-            event.target.value = "";
+            deleteBtn.onclick = (e) => {
+                deleteBtn.parentNode.remove();
+                event.target.style.border = "";
+                event.target.style.color = "black";
+                event.target.value = "";
+            }
         }
     }
 };
